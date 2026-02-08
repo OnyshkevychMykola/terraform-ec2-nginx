6fdc248886370ebee00b0b1b20883a9448b532e0
OnyshkevychMykola <mykola.onyshkevych@vitechteam.com>
2026-02-08 22:04:03 +0200

    added vpc

--- main.tf
+++ main.tf
@@ -12,9 +12,57 @@ provider "aws" {
   profile = "terraform"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "terraform-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_security_group" "nginx_sg" {
   name        = "nginx-sg"
   description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 22
@@ -39,11 +87,11 @@ resource "aws_security_group" "nginx_sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                         = "ami-0ecb62995f68bb549" # bubunda
-  instance_type               = "t3.micro"
-  key_name                    = "aws-key"
-  vpc_security_group_ids      = [aws_security_group.nginx_sg.id]
-  associate_public_ip_address = true
+  ami                    = "ami-0ecb62995f68bb549"
+  instance_type          = "t3.micro"
+  key_name               = "aws-key"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -58,3 +106,4 @@ resource "aws_instance" "web" {
     Name = "terraform-nginx"
   }
 }
+
